c420c0d3e83960c62de1cc2934de20af2eb9cd2f
Gabriel Gonçalves <gabrielgdev99@outlook.com>
2025-09-12 17:47:34 -0300

    input loginModel, contatoModel

--- CursoJS-TS-Luiz/back-end/aula19-projetoAgenda/src/controllers/contatoController.js
+++ CursoJS-TS-Luiz/back-end/aula19-projetoAgenda/src/controllers/contatoController.js
@@ -1,3 +1,25 @@
-exports.homePag = (req, res) => {
-  res.send("Obrigado por entrar em contato");
+const Contato = require("../models/contatoModel");
+
+exports.index = (req, res) => {
+  res.render("contato");
+};
+
+exports.register = async (req, res) => {
+  try {
+    const contato = new Contato(req.body);
+    await contato.register();
+
+    if (contato.errors.length > 0) {
+      req.flash("errors", contato.errors);
+      req.session.save(() => res.redirect("back"));
+      return;
+    }
+
+    req.flash("success", "Contato registrado com sucesso.");
+    req.session.save(() => res.redirect("back"));
+    return;
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
 };
