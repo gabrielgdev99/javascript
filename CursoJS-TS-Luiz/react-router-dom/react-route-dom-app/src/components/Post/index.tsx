3da3aa9d104baf42804937003660a26eb18c65e6
Gabriel Gonçalves <gabrielgdev99@outlook.com>
2025-11-11 22:37:44 -0300

    tutorial react router dom

---
+++ CursoJS-TS-Luiz/react-router-dom/react-route-dom-app/src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export const Post =() => {
+    return (
+        <div>
+            <h1>Abaixo o post</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti excepturi aspernatur corporis tempora dolorum laboriosam aut, doloremque consequatur recusandae numquam laudantium eius at voluptatum ad aliquid sequi ipsa in doloribus?</p>
+        </div>
+    )
+}
---
+++ CursoJS-TS-Luiz/react-router-dom/react-route-dom-app/src/components/Redirect/index.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from 'react-router-dom'
+import './style.css'
+import { useEffect, useRef, useState } from 'react'
+
+export const Redirect =() => {
+
+    const [time, setTime] = useState(3)
+    const timeOut = useRef(0)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        clearTimeout(timeOut.current)
+
+        timeOut.current = setTimeout(() => {
+            setTime(t => t - 1);
+;
+        }, 1000)
+
+        if (time <= 0 ) {
+            navigate('/about', {
+                state: `This is the state: ${Math.random()}`
+            })
+        }
+
+        return() => {
+        clearTimeout(timeOut.current)
+        }
+    }, [time])
+
+    return (
+        <div>
+            <h1>Get out of here in: {time}</h1>
+        </div>
+    )
+}
